971576a64dd365f50f3944d9bd481edbf42c43cc
Jan Aleksandrov <jan.aleksandrov@gmail.com>
2021-05-08 13:47:01 +0200

    Initial commit

---
+++ app/src/main/java/com/niproblema/emojicount/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.niproblema.emojicount
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
